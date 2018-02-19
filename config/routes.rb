58263fb4c0ea8b47a66537ebf8c4da47b928dfeb
Thomas Guwor <guworthomas@gmail.com>
2018-02-19 03:08:42 +0000

    add routes for for storefront

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+
   get 'storefronts/all_items'
 
-  get 'storefronts/items_by_category'
+#  get 'storefronts/items_by_category'
 
-  get 'storefronts/items_by_brand'
+#  get 'storefronts/items_by_brand'
+  
+  root 'storefronts#all_items'
 
+  get 'categorical' => 'storefronts#items_by_category' 
+
+  get 'branding' => 'storefronts#items_by_brand'
+  
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
